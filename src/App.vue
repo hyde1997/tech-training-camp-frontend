028b446935b7a4d5d3d214dc0a4c2d332a9b2112
hyde1997 <8054962+Hyde1997@user.noreply.gitee.com>
2021-02-28 17:11:02 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,15 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "app",
+  components: {},
+};
+</script>
+
 <style>
+@import "assets/css/base.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
